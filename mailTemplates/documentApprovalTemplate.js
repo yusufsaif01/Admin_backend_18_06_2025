eb2244a55065e0c1ac4d68e6e0f4103c66889326
akshay_balguhare <akshay.balguhare@4thpointer.com>
2020-06-15 15:06:00 +0530

    uppercase approval/disapproval email subject

--- mailTemplates/documentApprovalTemplate.js
+++ mailTemplates/documentApprovalTemplate.js
@@ -1,10 +1,10 @@
 const DOCUMENT_TYPE = require('../constants/DocumentType')
 module.exports = ({ email, documentType, name, memberType }) => {
-  documentType = documentType === DOCUMENT_TYPE.AADHAR ? "aadhaar" : documentType;
+  documentType = documentType === DOCUMENT_TYPE.AADHAR ? "Aadhaar" : (documentType.charAt(0).toUpperCase() + documentType.slice(1));
   return {
     to: email,
     subject: `${documentType} document details is verified`,
     // html: "",
-    text: `${documentType.charAt(0).toUpperCase() + documentType.slice(1)} document details for ${name} has been approved successfully by YFTChain.`,
+    text: `${documentType} document details for ${name} has been approved successfully by YFTChain.`,
   };
 };
--- mailTemplates/documentDisApprovalTemplate.js
+++ mailTemplates/documentDisApprovalTemplate.js
@@ -1,10 +1,10 @@
 const DOCUMENT_TYPE = require('../constants/DocumentType')
 module.exports = ({ email, documentType, name, memberType, reason }) => {
-  documentType = documentType === DOCUMENT_TYPE.AADHAR ? "aadhaar" : documentType;
+  documentType = documentType === DOCUMENT_TYPE.AADHAR ? "Aadhaar" : (documentType.charAt(0).toUpperCase() + documentType.slice(1));
   return {
     to: email,
     subject: `${documentType} document details is disapproved`,
     // html: "",
-    text: `${documentType.charAt(0).toUpperCase() + documentType.slice(1)} document details for ${name} has been disapproved by YFTChain due to "${reason}", Please update again.`,
+    text: `${documentType} document details for ${name} has been disapproved by YFTChain due to "${reason}", Please update again.`,
   };
 };
