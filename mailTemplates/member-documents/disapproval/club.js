c8116cd77eca9c9f493d932b5d316668e8d26b5d
Aman Kharbanda <aman.kharbanda@4thpointer.com>
2020-07-15 16:47:45 +0530

    implemented templates for - 1072 - 1073 - 1074 - 1075 - 1076 - 1077

--- mailTemplates/member-documents/disapproval/club.js
+++ mailTemplates/member-documents/disapproval/club.js
@@ -4,8 +4,102 @@ module.exports = ({ email, name, member_type, reason, from_email }) => {
     subject: `${name} document details disapproval`,
     html() {
       return `
-      ${reason}
-      ${from_email}
+      <tbody style="display: block;width: 80%; margin:auto;">
+			<tr style="height: 20px;">
+				<td></td>
+			</tr>
+
+			<!-- paragraph -->
+			<tr style="height: 20px;">
+				<td></td>
+			</tr>
+			<tr style="display:block; width: 100%;text-align: center;">
+				<td style="display:block; width: 100%; text-align: center;">
+					<p style="font-family: 'Montserrat', sans-serif;
+					font-size: 16px;font-weight: 300;color:#626262;
+					">
+						Hello Admin
+					</p>
+				</td>
+			</tr>
+
+			<tr style="height: 10px;">
+				<td></td>
+			</tr>
+
+			<tr style="display:block; width: 100%;text-align: center;">
+				<td style="display:block; width: 100%; text-align: center;">
+					<p style="font-family: 'Montserrat', sans-serif;
+					font-size: 16px;font-weight: 300;color:#626262;
+					">
+						Accreditation document details submitted by the following club have been disapproved through the system.
+					</p>
+				</td>
+			</tr>
+
+			<tr style="height: 10px;">
+				<td></td>
+			</tr>
+
+			<tr style="display:block; width: 100%;text-align: center;">
+				<td style="display:block; width: 100%; text-align: center;">
+					<p style="font-family: 'Montserrat', sans-serif;
+					font-size: 20px; font-weight: 700;display:block;color:#626262;">
+						Club Name: ${name}
+					</p>
+				</td>
+			</tr>
+
+			<tr style="height: 10px;">
+				<td></td>
+			</tr>
+
+			<tr style="display:block; width: 100%;text-align: center;">
+				<td style="display:block; width: 100%; text-align: center;">
+					<p style="font-family: 'Montserrat', sans-serif;
+					font-size: 20px; font-weight: 700;display:block;color:#626262;">
+						Member email id: ${from_email}
+					</p>
+				</td>
+			</tr>
+
+			<tr style="height: 10px;">
+				<td></td>
+			</tr>
+
+			<tr style="display:block; width: 100%;text-align: center;">
+				<td style="display:block; width: 100%; text-align: center;">
+					<p style="font-family: 'Montserrat', sans-serif;
+					font-size: 20px; font-weight: 700;display:block;color:#626262;">
+						Reason: ${reason}
+					</p>
+				</td>
+			</tr>
+
+
+			<tr style="height: 40px;">
+				<td></td>
+			</tr>
+
+			<!-- See you online -->
+
+			<tr style="display:block; width: 100%;text-align: center;">
+				<td style=" display:block; width: 100%; text-align: center;">
+					<p style="font-family: 'Montserrat', sans-serif;
+					font-size: 18px;font-weight: 300;color:#626262;
+					">
+						See you online!
+					</p>
+					<p style="font-family: 'Montserrat', sans-serif;
+					font-size: 20px; font-weight: 700;display:block;color:#626262;
+					">YFTChain Team</p>
+				</td>
+			</tr>
+			<tr style=" height: 20px;">
+				<td></td>
+			</tr>
+			<!-- end  -->
+		</tbody>
       `;
     },
   };
